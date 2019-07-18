0b6dec7beb5e7a204de21209b49cda6850efb2b4
wupeiheng <825895523@qq.com>
2019-07-18 21:29:16 +0800

    李四修改了User

---
+++ day39_es_demo/src/main/java/com/apple/pojo/User.java
@@ -0,0 +1,43 @@
+package com.apple.pojo;
+
+/**
+ * Package: com.apple.pojo
+ * ClassName:User
+ * date: 2019/7/18 21:15
+ *
+ * @author:吴沛恒
+ * @version:1.0
+ */
+public class User {
+    private int id;
+    private String name;
+    private boolean sex;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public void isMan(){
+        System.out.println(sex == true? "男":"女");
+    }
+}
