6c992328b5d4ead6bee7723c0ef590380ac2251e
wupeiheng <825895523@qq.com>
2019-07-18 22:30:17 +0800

    张三修改了User

--- day39_es_demo/src/main/java/com/apple/pojo/User.java
+++ day39_es_demo/src/main/java/com/apple/pojo/User.java
@@ -3,7 +3,6 @@ package com.apple.pojo;
 /**
  * Package: com.apple.pojo
  * ClassName:User
- * date: 2019/7/18 22:21
  *
  * @author:吴沛恒
  * @version:1.0
@@ -13,28 +12,7 @@ public class User {
     private String name;
     private boolean sex;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean isSex() {
-        return sex;
-    }
-
-    public void setSex(boolean sex) {
-        this.sex = sex;
-    }
-
-<<<<<<< HEAD
-    public void myName(){
-        System.out.println("名字是:"+name);
-=======
-    public void isMan(){
-        System.out.println(sex == true? "男":"女");
->>>>>>> origin/master
+    public void myName() {
+        System.out.println("名字是:" + name);
     }
 }
